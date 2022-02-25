0fc760118f784cf6c64fdb63d7e4c781336d024c
Arnav Singhal <singhal.arnav@gmail.com>
2022-02-26 01:35:59 +0530

    Implemented diagonal matching too!

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from math import fabs
 import numpy as np
+
+# Variable 
+TnoColumns = 7
+TnoRows = 6
+
+# Extra
+COLUMN_COUNT = TnoColumns - 1
+ROW_COUNT = TnoRows
+game = True
 
 def createBoard():
     return np.zeros((6,7))
@@ -17,21 +25,32 @@ def nextOpenRow(board, col):
 
 def winningMove(board, piece):
     # For horizontal wins
-    for i in range(4): # 4 because we cant have a win end of 3 in a row
-        for j in range(6):  
+    for i in range(COLUMN_COUNT-3):
+        for j in range(ROW_COUNT):  
             if board[i][j] == piece and board[i][j+1] == piece and board[i][j+2] == piece and board[i][j+3] == piece:
                 return True
     
     # For vertical wins
-    for i in range(6):
-        for j in range(3):
+    for i in range(COLUMN_COUNT):
+        for j in range(ROW_COUNT-3):
             if board[i][j] == piece and board[i+1][j] == piece and board[i+2][j] == piece and board[i+3][j] == piece:
+                return True
+    
+    # For this / diaganol  wins
+    for i in range(COLUMN_COUNT-3):
+        for j in range(ROW_COUNT-3):
+            if board[i][j] == piece and board[i+1][j+1] == piece and board[i+2][j+2] == piece and board[i+3][j+3] == piece:
+                return True
+    
+    # For this \ diaganol wins
+    for i in range(COLUMN_COUNT-3):
+        for j in range(3, ROW_COUNT):
+            if board[i][j] == piece and board[i-1][j+1] == piece and board[i-2][j+2] == piece and board[i-3][j+3] == piece:
                 return True
 
 def printBoard(board):
     print(np.flip(board, 0))
 
-game = True
 printBoardVar = True
 
 # Create a board
