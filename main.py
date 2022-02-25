a313fa3e486164d975fa93da1d012c88866f380f
Arnav Singhal <singhal.arnav@gmail.com>
2022-02-26 01:24:07 +0530

    Game works! (No GUI tho)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import fabs
 import numpy as np
 
 def createBoard():
@@ -14,15 +15,30 @@ def nextOpenRow(board, col):
         if board[i][col] == 0:
             return i
 
+def winningMove(board, piece):
+    # For horizontal wins
+    for i in range(4): # 4 because we cant have a win end of 3 in a row
+        for j in range(6):  
+            if board[i][j] == piece and board[i][j+1] == piece and board[i][j+2] == piece and board[i][j+3] == piece:
+                return True
+    
+    # For vertical wins
+    for i in range(6):
+        for j in range(3):
+            if board[i][j] == piece and board[i+1][j] == piece and board[i+2][j] == piece and board[i+3][j] == piece:
+                return True
+
 def printBoard(board):
     print(np.flip(board, 0))
 
-game = False
+game = True
+printBoardVar = True
+
 # Create a board
 board = createBoard()
 turn = 0
 
-while not game:
+while game:
     # Player 1 turn
     if turn == 0:
         col = int(input("Player 1, choose a column: "))
@@ -32,6 +48,12 @@ while not game:
                 row = nextOpenRow(board, col)
                 dropPiece(board, row, col, 1)
                 turn = 1
+                printBoardVar = True
+
+                if winningMove(board, 1):
+                    print("Player 1 wins!")
+                    game = False
+                    printBoardVar = False
         else:
             print("Enter valid command!")
 
@@ -42,8 +64,9 @@ while not game:
             if isValidMove(board, col):
                 row = nextOpenRow(board, col)
                 dropPiece(board, row, col, 2)
-                turn = 1
+                turn = 0
         else:
             print("Enter valid command!")
 
-    printBoard(board)
+    if printBoardVar:
+        printBoard(board)
